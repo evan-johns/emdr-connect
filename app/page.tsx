0149aad97e75be4c0150393c9fb2cff4af09fe3c
evan-johns <evandjohns@gmail.com>
2023-08-31 21:36:12 -0400

    updated landing page with magic link sign up

--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,21 @@ import AuthForm from "./auth-form";
 
 export default function Home() {
   return (
-    <div className="row">
-      <div className="col-6">
-        <h1 className="header">Supabase Auth + Storage</h1>
-        <p className="">
-          Experience our Auth and Storage through a simple profile management
-          example. Create a user profile and upload an avatar image. Fast,
-          simple, secure.
-        </p>
-      </div>
-      <div className="col-6 auth-widget">
-        <AuthForm />
+    <div className="container d-flex justify-content-center align-items-center vh-100">
+      <div className="row">
+        <div className="col-12 text-center mb-4">
+          <h1 className="header font-bold text-lg">EMDR Connect</h1>
+          <p>
+            Connect securely with your client from anywhere for a telehealth
+            EMDR therapy session.<br></br> Send a "magic link" to your email
+            below to sign in.
+          </p>
+        </div>
+        <div className="col-12 d-flex justify-content-center">
+          <div className="auth-widget">
+            <AuthForm />
+          </div>
+        </div>
       </div>
     </div>
   );
